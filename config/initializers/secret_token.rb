d2c0a237bca17ef2e877236f88938d246d874802
vitorbenedito <vitor.benedito@gmail.com>
2012-06-24 14:07:58 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ceam::Application.config.secret_token = '98d028505301721ace7062c6f2905c95091c80f6105c1a82af489d290361517d814ac2f6ee461b32f19baed19ddd9e50811deebab557ad22309d5c81e85ec788'
